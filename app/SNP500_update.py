d3225975bcfd5be743afde712b00e0f0fba92933
sklee <nohvenell2@gmail.com>
2024-10-30 18:46:33 +0900

    주석 수정

--- app/SNP500_update.py
+++ app/SNP500_update.py
@@ -13,7 +13,7 @@ def main(dprint : bool = False,tqdm_disable : bool = True, past = PAST):
     update_info(SYMBOLS,tqdm_disable=tqdm_disable)
     update_price_daily(SYMBOLS, tqdm_disable = tqdm_disable, past = past)
     end_time = datetime.now()
-    if dprint: print(f'[END - {end_time}] ----------- S&P500 Update. Take ')
+    if dprint: print(f'[END - {end_time}] ----------- S&P500 Update')
     execution_time = end_time - start_time
     if dprint: print(f'Excution Time : {execution_time}')
     
