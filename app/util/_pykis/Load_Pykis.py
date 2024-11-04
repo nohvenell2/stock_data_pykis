7b392d635ff8a169a6139657a851611c8c8d538f
sklee <nohvenell2@gmail.com>
2024-11-04 16:36:51 +0900

    토큰 획득과정 수정

--- app/util/_pykis/Load_Pykis.py
+++ app/util/_pykis/Load_Pykis.py
@@ -1,3 +1,15 @@
 from dotenv import load_dotenv
+import os
 from pykis import PyKis
-KIS = PyKis("secret.json", keep_token=True)
+load_dotenv()
+SITE_ID=os.getenv('SITE_ID')
+ACC_NO_REAL=os.getenv('ACC_NO_REAL')
+APP_KEY_REAL=os.getenv('APP_KEY_REAL')
+APP_SEC_REAL=os.getenv('APP_SEC_REAL')
+KIS = PyKis(
+    id=SITE_ID,
+    account=ACC_NO_REAL,
+    appkey=APP_KEY_REAL,
+    secretkey=APP_SEC_REAL,
+    keep_token=True
+)
